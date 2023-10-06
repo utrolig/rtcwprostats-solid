1ac401b43b69e36cf690ab28d0dbc39bc5d290d3
Stian Bakken <sbakken@gmail.com>
2023-10-06 22:11:12 +0200

    hoist sorting props

--- src/components/Groups/Body/Body.tsx
+++ src/components/Groups/Body/Body.tsx
@@ -1,13 +1,124 @@
-import { GroupsResponse } from "~/api/types";
+import { Faction, GroupsResponse } from "~/api/types";
 import styles from "./Body.module.css";
-import { For } from "solid-js";
+import { For, createSignal } from "solid-js";
 import { TeamTable } from "~/components/TeamTable/TeamTable";
+import {
+  SortDir,
+  TableRowSortKey,
+  getAccuracy,
+  getAdd,
+  getKdr,
+} from "~/utils/utils";
+import { getTeamFromFaction, groupsResponseToTeams } from "~/utils/teams";
+import { getPlayersFromTeam } from "~/utils/players";
 
 export type BodyProps = {
   data: GroupsResponse;
 };
 
 export default function Body(props: BodyProps) {
+  const [sortKey, setSortKey] = createSignal<TableRowSortKey>(
+    TableRowSortKey.Kdr
+  );
+  const [sortDir, setSortDir] = createSignal<SortDir>("desc");
+
+  const onSortClicked = (key: TableRowSortKey) => {
+    if (sortKey() === key) {
+      if (sortDir() === "asc") {
+        setSortDir("desc");
+      } else {
+        setSortDir("asc");
+      }
+
+      return;
+    }
+
+    setSortKey(key);
+  };
+
+  const getTeam = (faction: Faction) => {
+    const teams = groupsResponseToTeams(props.data);
+    return getTeamFromFaction(teams.factions, faction);
+  };
+
+  const getPlayers = (faction: Faction) => {
+    const team = getTeam(faction);
+    const players = getPlayersFromTeam(team, props.data);
+    console.log("sortKey", sortKey());
+    return [...players].sort((a, b) => {
+      switch (sortKey()) {
+        case TableRowSortKey.Accuracy: {
+          return (
+            (getAccuracy(a) - getAccuracy(b)) * (sortDir() === "asc" ? 1 : -1)
+          );
+        }
+
+        case TableRowSortKey.Add: {
+          return (getAdd(a) - getAdd(b)) * (sortDir() === "asc" ? 1 : -1);
+        }
+
+        case TableRowSortKey.DamageDone: {
+          return (
+            (a.categories.damagegiven - b.categories.damagegiven) *
+            (sortDir() === "asc" ? 1 : -1)
+          );
+        }
+
+        case TableRowSortKey.DamageReceived: {
+          return (
+            (a.categories.damagereceived - b.categories.damagereceived) *
+            (sortDir() === "asc" ? 1 : -1)
+          );
+        }
+
+        case TableRowSortKey.Deaths: {
+          return (
+            (a.categories.deaths - b.categories.deaths) *
+            (sortDir() === "asc" ? 1 : -1)
+          );
+        }
+
+        case TableRowSortKey.Gibs: {
+          return (
+            (a.categories.gibs - b.categories.gibs) *
+            (sortDir() === "asc" ? 1 : -1)
+          );
+        }
+
+        case TableRowSortKey.Headshots: {
+          return (
+            (a.categories.headshots - b.categories.headshots) *
+            (sortDir() === "asc" ? 1 : -1)
+          );
+        }
+
+        case TableRowSortKey.Kills: {
+          return (
+            (a.categories.kills - b.categories.kills) *
+            (sortDir() === "asc" ? 1 : -1)
+          );
+        }
+
+        case TableRowSortKey.Kdr: {
+          return (getKdr(a) - getKdr(b)) * (sortDir() === "asc" ? 1 : -1);
+        }
+
+        case TableRowSortKey.Revives: {
+          return (
+            (a.categories.revives - b.categories.revives) *
+            (sortDir() === "asc" ? 1 : -1)
+          );
+        }
+
+        case TableRowSortKey.Name: {
+          return (
+            a.alias.localeCompare(b.alias) * (sortDir() === "asc" ? 1 : -1)
+          );
+        }
+      }
+    });
+  };
+
   return (
     <div class={styles.body}>
       <ul class={styles.tabs}>
@@ -23,8 +134,22 @@ export default function Body(props: BodyProps) {
           )}
         </For>
       </ul>
-      <TeamTable groupsData={props.data} faction={"Allied"} />
-      <TeamTable groupsData={props.data} faction={"Axis"} />
+      <TeamTable
+        players={getPlayers("Allied")}
+        sortKey={sortKey()}
+        sortDir={sortDir()}
+        onSortClicked={onSortClicked}
+        groupsData={props.data}
+        faction={"Allied"}
+      />
+      <TeamTable
+        players={getPlayers("Axis")}
+        sortKey={sortKey()}
+        sortDir={sortDir()}
+        onSortClicked={onSortClicked}
+        groupsData={props.data}
+        faction={"Axis"}
+      />
     </div>
   );
 }
--- src/components/TeamTable/TeamTable.tsx
+++ src/components/TeamTable/TeamTable.tsx
@@ -1,128 +1,23 @@
-import { Faction, GroupsResponse, PlayerStats } from "~/api/types";
+import { Faction, GroupsResponse } from "~/api/types";
 import styles from "./TeamTable.module.css";
 import { factionImages } from "~/assets/factionImages";
 import { toReadableFaction } from "~/utils/faction";
 import { TablePlayerRow } from "./TablePlayerRow/TablePlayerRow";
-import { getTeamFromFaction, groupsResponseToTeams } from "~/utils/teams";
-import { getPlayersFromTeam } from "~/utils/players";
-import { For, createSignal } from "solid-js";
+import { PlayerStatsWithClass } from "~/utils/teams";
+import { For } from "solid-js";
 import { TablePlayerHeaderRow } from "./TablePlayerRow/TablePlayerHeaderRow";
-import {
-  SortDir,
-  TableRowSortKey,
-  getAccuracy,
-  getAdd,
-  getKdr,
-} from "~/utils/utils";
+import { SortDir, TableRowSortKey } from "~/utils/utils";
 
 type TeamTableProps = {
   groupsData: GroupsResponse;
   faction: Faction;
+  sortDir: SortDir;
+  sortKey: TableRowSortKey;
+  onSortClicked: (key: TableRowSortKey) => void;
+  players: PlayerStatsWithClass[];
 };
 
 export const TeamTable = (props: TeamTableProps) => {
-  const [sortKey, setSortKey] = createSignal<TableRowSortKey>(
-    TableRowSortKey.Kdr
-  );
-  const [sortDir, setSortDir] = createSignal<SortDir>("desc");
-
-  const getTeam = () => {
-    const teams = groupsResponseToTeams(props.groupsData);
-    return getTeamFromFaction(teams.factions, props.faction);
-  };
-
-  const getPlayers = () => {
-    const team = getTeam();
-    const players = getPlayersFromTeam(team, props.groupsData);
-    console.log("sortKey", sortKey());
-    return [...players].sort((a, b) => {
-      switch (sortKey()) {
-        case TableRowSortKey.Accuracy: {
-          return (
-            (getAccuracy(a) - getAccuracy(b)) * (sortDir() === "asc" ? 1 : -1)
-          );
-        }
-
-        case TableRowSortKey.Add: {
-          return (getAdd(a) - getAdd(b)) * (sortDir() === "asc" ? 1 : -1);
-        }
-
-        case TableRowSortKey.DamageDone: {
-          return (
-            (a.categories.damagegiven - b.categories.damagegiven) *
-            (sortDir() === "asc" ? 1 : -1)
-          );
-        }
-
-        case TableRowSortKey.DamageReceived: {
-          return (
-            (a.categories.damagereceived - b.categories.damagereceived) *
-            (sortDir() === "asc" ? 1 : -1)
-          );
-        }
-
-        case TableRowSortKey.Deaths: {
-          return (
-            (a.categories.deaths - b.categories.deaths) *
-            (sortDir() === "asc" ? 1 : -1)
-          );
-        }
-
-        case TableRowSortKey.Gibs: {
-          return (
-            (a.categories.gibs - b.categories.gibs) *
-            (sortDir() === "asc" ? 1 : -1)
-          );
-        }
-
-        case TableRowSortKey.Headshots: {
-          return (
-            (a.categories.headshots - b.categories.headshots) *
-            (sortDir() === "asc" ? 1 : -1)
-          );
-        }
-
-        case TableRowSortKey.Kills: {
-          return (
-            (a.categories.kills - b.categories.kills) *
-            (sortDir() === "asc" ? 1 : -1)
-          );
-        }
-
-        case TableRowSortKey.Kdr: {
-          return (getKdr(a) - getKdr(b)) * (sortDir() === "asc" ? 1 : -1);
-        }
-
-        case TableRowSortKey.Revives: {
-          return (
-            (a.categories.revives - b.categories.revives) *
-            (sortDir() === "asc" ? 1 : -1)
-          );
-        }
-
-        case TableRowSortKey.Name: {
-          return (
-            a.alias.localeCompare(b.alias) * (sortDir() === "asc" ? 1 : -1)
-          );
-        }
-      }
-    });
-  };
-
-  const onSortClicked = (key: TableRowSortKey) => {
-    if (sortKey() === key) {
-      if (sortDir() === "asc") {
-        setSortDir("desc");
-      } else {
-        setSortDir("asc");
-      }
-
-      return;
-    }
-
-    setSortKey(key);
-  };
-
   return (
     <div class={styles.container}>
       <div class={styles.header}>
@@ -131,11 +26,11 @@ export const TeamTable = (props: TeamTableProps) => {
       </div>
       <div class={styles.body}>
         <TablePlayerHeaderRow
-          sortKey={sortKey()}
-          sortDir={sortDir()}
-          onSortClicked={onSortClicked}
+          sortKey={props.sortKey}
+          sortDir={props.sortDir}
+          onSortClicked={props.onSortClicked}
         />
-        <For each={getPlayers()}>
+        <For each={props.players}>
           {(player) => (
             <TablePlayerRow
               playerElos={props.groupsData.elos}
